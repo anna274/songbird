1eb04018c942abeb8a1d0113d8db85d15837ec58
anna274 <anna_rusakovich.01@mail.ru>
2020-08-02 22:30:19 +0300

    feat: add level 1 data

--- my-app/src/Question.js
+++ my-app/src/Question.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import './App.css';
 
+const DEFAULT_IMG_URL = '/images/musician.png';
+
 class Question extends React.Component {
   render() {
     if( this.props.levelCompleted ) {
       return (
         <div className="question">
           <div className="question__img-container">
-            <img className="question__img" alt="" src={this.props.songImg} />
+            <img className="question__img" alt="" src={this.props.question.songImg} />
           </div>
           <div>
-            <h3 className="question__title">{ this.props.singer + ' - ' + this.props.songName}</h3>
+            <h3 className="question__title">{ this.props.question.singer + ' - ' + this.props.question.songName}</h3>
             <audio controls>
-              <source src={ this.props.question.songAudio } />
+              <source src={ process.env.PUBLIC_URL + this.props.question.songAudio } />
             </audio>
           </div>
         </div>
@@ -21,11 +23,12 @@ class Question extends React.Component {
       return (
         <div className="question">
           <div className="question__img-container">
+            <img className="question__img" alt="" src={ process.env.PUBLIC_URL + DEFAULT_IMG_URL } />
           </div>
           <div>
             <h3 className="question__title">*******</h3>
             <audio controls>
-              <source src={ this.props.songAudio } />
+              <source src={ process.env.PUBLIC_URL + this.props.question.songAudio } />
             </audio>
           </div>
         </div>
